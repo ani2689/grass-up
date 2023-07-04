1ffeb1839e1da1a0bae696700610d5cb2c212358
ani2689 <ojno7777@gmail.com>
2023-07-04 20:57:23 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/ani/grassup/GrassUpApplication.java
@@ -0,0 +1,13 @@
+package com.ani.grassup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrassUpApplication.class, args);
+    }
+
+}
